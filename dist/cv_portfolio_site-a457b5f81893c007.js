31d3b0456073a23105255fec8ce39ea9b7bc3563
Firat Honca <firathonca@gmail.com>
2024-09-04 16:06:40 +0100

    Version changes

--- dist/cv_portfolio_site-a457b5f81893c007.js
+++ dist/cv_portfolio_site-a457b5f81893c007.js
@@ -626,12 +626,12 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper3505 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 249, __wbg_adapter_20);
+    imports.wbg.__wbindgen_closure_wrapper3520 = function(arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 253, __wbg_adapter_20);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper4742 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 326, __wbg_adapter_23);
+    imports.wbg.__wbindgen_closure_wrapper4757 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 330, __wbg_adapter_23);
         return addHeapObject(ret);
     };
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 use yew::prelude::*;
-// use yew_layout::{
-//     Align, AlignRows, Column, CrossAlign, Gap, Length, Margin, Overflow, Padding, Row
-// };
 use pulldown_cmark::{html, Parser};
 
 fn create_markdown(markdown_input: &str) -> Html {
@@ -50,22 +47,47 @@ Custom Excel workflow for Apple titles to create labels for outgoing data.\
  
 }
  
-fn h2_test() -> Html {
-    let markdown = r#"
-## Test
-    "# ; 
-    create_markdown(&markdown) 
+const LOGO_SIZE: &str = "44px"; 
  
+// fn add_html_picture(site_url: &str,site_logo_url: &str){
+//     html!(
+//         
+//     )
+// }
+
+fn firat_header() -> Html {
+    let page_title = "Firat Honca";
+    let github_url = "https://github.com/Mightybeast12";
+    let linkedin_url = "https://www.linkedin.com/in/firathonca"; 
+    let github_logo_url = "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"; 
+    let linkedin_logo_url = "https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"; 
+
+    html!(
+        <div style="display: flex; justify-content: space-between; align-items: center;">
+            <h2 style="color: rgb(209, 167, 111); font-size: 48px;">{ page_title }</h2>
+            <div style="display: flex; gap: 15px;">
+                <a href={github_url} target="_blank" rel="noopener noreferrer">
+                    <img src={github_logo_url} alt="GitHub Logo" style={format!("width: {}; height: {}; ", LOGO_SIZE, LOGO_SIZE)} />
+                </a>
+                <a href={linkedin_url} target="_blank" rel="noopener noreferrer">
+                    <img src={linkedin_logo_url} alt="LinkedIn Logo" style={format!("width: {}; height: {}; ", LOGO_SIZE, LOGO_SIZE)} />
+                </a>
+            </div>
+        </div>
+    )
 }
+
   
 #[function_component]
 fn App() -> Html {
     html! {
         <div> 
-            {create_inspekt_markdown()}
-            {create_alula_auto_ingest_markdown()}
-            {create_alula_bible_markdown()}
-            {h2_test()}
+        <h1> {firat_header()} </h1> 
+            <div> 
+                {create_inspekt_markdown()}
+                {create_alula_auto_ingest_markdown()}
+                {create_alula_bible_markdown()}
+            </div> 
         </div> 
     }
 }
