f0c44d8c11a736e86a1f480df8b21c6dc3cc8944
Firat Honca <firathonca@gmail.com>
2024-09-09 15:01:33 +0100

    changes

--- src/page_builder/code_show_case_elements.rs
+++ src/page_builder/code_show_case_elements.rs
@@ -3,7 +3,14 @@ use crate::utils::card_elements;
 
 pub fn rust_docker_container_showcase() -> Html {
     let title = "Rust Docker".to_string(); 
-    let file = "static/Docker Build".to_string();
+    let file = "static/cv_portfolio/Rust_Dockerfile ".to_string();
+    card_elements::create_dynamic_styled_markdown(title, file) 
+    
+}
+ 
+pub fn auto_ingest_docker_container_showcase() -> Html {
+    let title = "Auto Ingest Dockerfile".to_string(); 
+    let file = "static/AutoIngest/AutoIngest Dockerfile".to_string();
     card_elements::create_dynamic_styled_markdown(title, file) 
     
 }
--- src/page_builder/home_page.rs
+++ src/page_builder/home_page.rs
@@ -44,14 +44,15 @@ Custom Excel workflow for Apple titles to create labels for outgoing data.\
 }
 
 pub fn home_page() -> Html{
-    let file = "static/Notification.py".to_string(); 
+    let file = "static/AutoIngest/Notification.py".to_string(); 
     html!(
         <div>
-        {header_builder::build_header()} 
-        {create_inspekt_markdown()}
-        {create_alula_auto_ingest_markdown()}
-        {create_alula_bible_markdown()}
-        {card_elements::create_dynamic_styled_markdown("Notification - Inspet".to_string(),file)} 
+            {header_builder::build_header()} 
+            {create_inspekt_markdown()}
+            {create_alula_auto_ingest_markdown()}
+            {create_alula_bible_markdown()}
+            {card_elements::create_dynamic_styled_markdown("Notification - Inspet".to_string(),file)} 
+            {code_show_case_elements::auto_ingest_docker_container_showcase()} 
             {code_show_case_elements::rust_docker_container_showcase()} 
         </div>
     )
