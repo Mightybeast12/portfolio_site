49782733bf195b005b28cdcd86d1d4239c6fdf04
Firat Honca <firathonca@gmail.com>
2025-09-05 15:26:35 +0100

    changes to see if push

--- src/page_builder/home_page.rs
+++ src/page_builder/home_page.rs
@@ -25,7 +25,7 @@ pub fn home_page() -> Html {
                 <div class="button-20-container">
                     <Link<Route> to={Route::PortfolioSitePage} classes="button-link">
                         <div class="button-20">
-                            <span class="button-20-text">{"Test auto CHECK OUT"}</span>
+                            <span class="button-20-text">{"Test auto 2 CHECK OUT"}</span>
                             <span class="button-20-arrow"></span>
                         </div>
                     </Link<Route>>
