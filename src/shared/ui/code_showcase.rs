035050ee3343217bdc3e9e7f12bfd266aeceb2aa
Firat Honca <firathonca@gmail.com>
2025-09-07 08:47:58 +0100

    Bug fix

--- src/shared/ui/code_showcase.rs
+++ src/shared/ui/code_showcase.rs
@@ -3,7 +3,7 @@ use yew::prelude::*;
 
 pub fn rust_docker_container_showcase() -> Html {
     let title = "Rust Docker".to_string();
-    let file = "static/cv_portfolio/Rust_Dockerfile ".to_string();
+    let file = "static/cv_portfolio/Rust_Dockerfile".to_string();
     cards::create_dynamic_styled_markdown(title, file)
 }
 
